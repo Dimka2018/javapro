7b9c3705fe56529ca9819632b4b3a73cca1fbe78
dimka2018 <dmitryplotnikov.job@gmail.com>
2021-10-27 19:41:27 +0300

    initial commit

---
+++ src/test/java/com/dimka/javapro/JavaproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dimka.javapro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
